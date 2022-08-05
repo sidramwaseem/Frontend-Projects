495980416dd3f9c163f1f3666b7f31a2aa7670a4
Sidra Waseem <sidraw24@gmail.com>
2022-08-06 00:58:42 +0500

    Update card.jsx

--- API/Junior/advice-generator/src/components/Card.jsx
+++ API/Junior/advice-generator/src/components/Card.jsx
@@ -1,13 +1,21 @@
-import React from 'react';
-import './Card.css';
+import React from "react";
+import "./Card.css";
+import divider from '../assets/images/pattern-divider-desktop.svg';
+import dice from '../assets/images/icon-dice.svg';
 
 function Card(props) {
   return (
-    <div className="card">
-      <h1>Advice #{props.id}</h1>
-      <p>{props.advice}</p>
+    <div className="container">
+      <div className="container--card">
+        <h1 className="container--card-h1">Advice  #{props.id}</h1>
+        <p className="container--card-p">"{props.advice}"</p>
+      </div>
+      <img src={divider} alt="divider" className="divider" />
+      <div class="dice-container">
+      <img src={dice} alt="dice button" className="dice" />
+      </div>
     </div>
-  )
+  );
 }
 
-export default Card
+export default Card;
