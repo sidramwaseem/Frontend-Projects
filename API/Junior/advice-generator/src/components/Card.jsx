156bade43d4bbdbb2b8600b8b6706b8eef92b000
Sidra Waseem <sidraw24@gmail.com>
2022-08-06 01:12:16 +0500

    format code

--- API/Junior/advice-generator/src/components/Card.jsx
+++ API/Junior/advice-generator/src/components/Card.jsx
@@ -1,18 +1,18 @@
 import React from "react";
 import "./Card.css";
-import divider from '../assets/images/pattern-divider-desktop.svg';
-import dice from '../assets/images/icon-dice.svg';
+import divider from "../assets/images/pattern-divider-desktop.svg";
+import dice from "../assets/images/icon-dice.svg";
 
 function Card(props) {
   return (
     <div className="container">
       <div className="container--card">
-        <h1 className="container--card-h1">Advice  #{props.id}</h1>
+        <h1 className="container--card-h1">Advice #{props.id}</h1>
         <p className="container--card-p">"{props.advice}"</p>
       </div>
       <img src={divider} alt="divider" className="divider" />
       <div class="dice-container" onClick={props.toggleChange}>
-      <img src={dice} alt="dice button" className="dice" />
+        <img src={dice} alt="dice button" className="dice" />
       </div>
     </div>
   );
