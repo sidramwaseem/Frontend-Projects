024f943a4b8267edb3ffa92924603f738aa6463e
Sidra Waseem <sidraw24@gmail.com>
2022-08-06 00:10:33 +0500

    Fetch API

--- API/Junior/advice-generator/src/App.js
+++ API/Junior/advice-generator/src/App.js
@@ -1,12 +1,36 @@
-import React from 'react';
-import Card from './components/Card.jsx';
+import React from "react";
+import Card from "./components/Card.jsx";
+import "./App.css";
 
 function App() {
+  const [advice, setAdvice] = React.useState({
+    id:0,
+    advice:"",
+  });
+
+  React.useEffect(() => {
+    async function getAdvice() {
+      const res = await fetch(
+        "https://api.adviceslip.com/advice"
+      );
+      const data = await res.json();
+      setAdvice(() => {
+        return {
+          id: data.slip.id,
+          advice: data.slip.advice,
+        };
+      });
+    }
+    getAdvice();
+  }, []);
+
+  console.log(advice.advice);
+
   return (
     <div>
-      <Card />
+      <Card id={advice.id} advice={advice.advice} />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
